f6b009b2335cb703d37c4460eb82a5585420205a
Alec Barney <alecbarney34@gmail.com>
2022-03-15 10:38:54 -0600

    finished deployment set up

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import {useState} from 'react'
+
 
 function App() {
+  const [name, setName] = useState('')
+  const [price, setPrice] = useState('')
+
+  const accessDB = ()=>{
+    axios.get('/api/info')
+    .then((res) =>{
+      console.log(res.data)
+      setName(res.data.name)
+      setPrice(res.data.price)
+    })
+    .catch((err) =>{
+      console.log(err.response.data)
+    })
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>{name ? name : "No Product"}</h2>
+        <h3>{price ? price : "no price"}</h3>
+        <button onClick={accessDB}>access database</button>
       </header>
     </div>
   );
